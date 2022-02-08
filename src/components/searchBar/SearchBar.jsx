bbf6253e68086951f5fbf5184f895e9c05bedb01
~Gabriel <~gabriel.p1997@yahoo.com~>
2022-02-08 11:12:23 -0500

    update search bar

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -8,28 +8,34 @@ import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 function SearchBar() {
     const [result, setResult] = useState([])
+    // const [history, setHistory] = useState([])
     const [arrowPosition, setArrowPosition] = useState(-1)
     const [showList, setShowList] = useState(false)
+    const [query, setquery] = useState('')
     const refInput = useRef(null)
 
-    function bingWebSearch(query) {
-        const value = query.target.value.trim()
-        if (value == '') {
-            setResult([])
-            setShowList(false)
+
+    function bingWebSearch(value, e) {
+        if (!e) return
+        if (e.code == "ArrowDown" || e.code == "ArrowUp") {
             return
-        }
-        axios.get(`https://api.bing.microsoft.com/v7.0/Suggestions?q=${value}`, {
-            headers: { 'Ocp-Apim-Subscription-Key': process.env.REACT_APP_AZURE_SUBSCRIPTION_KEY }
-        }).then((result) => {
-            setResult(result.data.suggestionGroups[0].searchSuggestions)
-            setShowList(true)
-        })
-            .catch((err) => {
+        } else {
+
+            setquery(value)
+            if (value == '') {
+                setResult([])
+                setShowList(false)
+                return
+            }
+            axios.get(`https://api.bing.microsoft.com/v7.0/Suggestions?q=${value}`, {
+                headers: { 'Ocp-Apim-Subscription-Key': process.env.REACT_APP_AZURE_SUBSCRIPTION_KEY }
+            }).then((result) => {
+                setResult(result.data.suggestionGroups[0].searchSuggestions)
+                setShowList(true)
+            }).catch((err) => {
                 console.error(err)
             })
-
-
+        }
     }
     function closeButton() {
         refInput.current.value = ''
@@ -37,40 +43,92 @@ function SearchBar() {
         setShowList(false)
     }
     function arrowMove(e) {
-        if (e.key == 'ArrowDown') {
-            if (arrowPosition <= 6) {
+        if (refInput.current.value.trim() == '') return
+        const res = result
+        if (e.key === 'ArrowDown') {
+            if (arrowPosition < 7) {
                 setArrowPosition(arrowPosition + 1)
+            } else {
+                setArrowPosition(-1)
+                refInput.current.value = query
+                return
             }
+            if (arrowPosition == -1) {
+                refInput.current.value = result[arrowPosition + 1].displayText
+            } else {
+                refInput.current.value = result[arrowPosition + 1].displayText
+            }
+            setResult(res)
         }
-        if (e.key == 'ArrowUp') {
-            if (arrowPosition >= 1) {
+        if (e.key === 'ArrowUp') {
+            if (arrowPosition == -1) {
+                setArrowPosition(7)
+                refInput.current.value = refInput.current.value = result[7].displayText
+            } if (arrowPosition <= 7 && arrowPosition > 0) {
+                refInput.current.value = result[arrowPosition].displayText
                 setArrowPosition(arrowPosition - 1)
+            }if(arrowPosition === 0){
+                refInput.current.value = query
+                setArrowPosition(-1)
+
             }
+            setResult(res)
         }
-        if(e.key == 'Enter'){
+        if (e.key == 'ArrowRight') {
+            if (arrowPosition === -1) {
+                return
+            }
+            // setHistory(oldArray => [...oldArray, refInput.current.value]);
+            bingWebSearch(result[arrowPosition].displayText, false)
+            refInput.current.value = result[arrowPosition].displayText
+
+        }
+        // if (e.key == 'ArrowLeft') {
+        //     if (arrowPosition == -1) {
+        //         return
+        //     }
+        //     if(history.length == 0){
+        //         refInput.current.value = query
+        //         bingWebSearch(query, false)
+
+        //     }else{
+
+        //         refInput.current.value = history[history.length -1]
+        //         bingWebSearch(history[history.length -1], false)
+
+        //         history.pop()
+        //     }
+        // }
+        if (e.key === 'Enter') {
             const url = result[arrowPosition].url
             window.open(url, '_blank').focus();
         }
     }
 
-    function formatResult(text, length, url) {
+    function formatResult(text, url) {
         const split = text.split('')
         const result = []
+        const searched = []
+        const length = query.split('').length
         for (let i = length; i < split.length; i++) {
             result.push(split[i])
         }
+        for (let i = 0; i < length; i++) {
+            searched.push(split[i])
+        }
         result.join('')
+        searched.join('')
         return <span onClick={() => window.open(url, '_blank').focus()}>
             <span><FontAwesomeIcon icon={faSearch} /></span>
-            <span >{refInput.current.value}</span>
+            <span >{searched}</span>
             <span className='gray'>{result}</span>
         </span>
     }
 
     return <div className="search-input">
         <div className='input'>
-            <FontAwesomeIcon icon={faSearch} /> 
-            <input type="text" placeholder="Type to search.." onChange={(e) => bingWebSearch(e)} ref={refInput} onKeyDown={(e) => arrowMove(e)} />
+            <FontAwesomeIcon icon={faSearch} />
+            <input type="text" placeholder="Type to search.." onKeyUp={(e) => bingWebSearch(e.target.value.trim(), e)} ref={refInput} onKeyDown={(e) => arrowMove(e)}/>
             <span onClick={() => closeButton()}><FontAwesomeIcon icon={faTimes} /></span>
         </div>
         {showList ? <div className="autocom-box" >
@@ -78,7 +136,7 @@ function SearchBar() {
                 return <li key={val.displayText}
                     value={val.displayText}
                     className={i === arrowPosition ? 'select' : ''}>
-                    {formatResult(val.displayText, refInput.current.value.length, val.url)}
+                    {formatResult(val.displayText, val.url)}
                 </li>
             })}
         </div>
